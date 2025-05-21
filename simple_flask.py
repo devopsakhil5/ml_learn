5b77adba153eaaffc50841bd11ee580bed9d2d34
Akhil <110562970+devopsakhil5@users.noreply.github.com>
2025-05-21 14:28:14 +0000

    Flask app

---
+++ simple_flask.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><h1>Educational institute</h1></html>"
+
+@app.route("/courses")
+def courses():
+    return "Devops, Linux, python, docker"
+
+@app.route("/syllabus")
+def syllabus():
+    return render_template("syllabus.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
